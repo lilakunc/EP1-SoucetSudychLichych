bb6f12fc9dbd4180deadf5605d671819a7503b88
Linda Kuncová <Kuncova.Linda@stud.vassboskovice.cz>
2024-06-12 10:35:55 +0200

    lol!!

--- EP1-SoucetSudychLichych/Program.vb
+++ EP1-SoucetSudychLichych/Program.vb
@@ -4,24 +4,27 @@ Module Module1
     Sub Main()
         Console.WriteLine("Zadejte poèet èísel:")
         Dim pocetCisel As Integer = Convert.ToInt32(Console.ReadLine())
-        Dim cisla(pocetCisel - 1) As Integer
+        Dim soucetSudych As Integer = 0
+        Dim soucetLichych As Integer = 0
         For i As Integer = 0 To pocetCisel - 1
             Dim cislo As Integer
             Do
                 Console.WriteLine("Zadejte nezáporné èíslo " & (i + 1) & ":")
                 cislo = Convert.ToInt32(Console.ReadLine())
                 If cislo >= 0 Then
-                    cisla(i) = cislo
+                    If cislo Mod 2 = 0 Then
+                        soucetSudych += cislo
+                    Else
+                        soucetLichych += cislo
+                    End If
                     Exit Do
                 Else
                     Console.WriteLine("Zadaná hodnota musí být nezáporná. Zkus to znovu prosím.")
                 End If
             Loop
         Next
-        Console.WriteLine("Zadali jste následující èísla:")
-        For Each cislo As Integer In cisla
-            Console.WriteLine(cislo)
-        Next
+        Console.WriteLine("Souèet sudých èísel: " & soucetSudych)
+        Console.WriteLine("Souèet lichých èísel: " & soucetLichych)
         Console.WriteLine("Stisknìte jakoukoli klávesu pro ukonèení programu.")
         Console.ReadKey()
     End Sub
